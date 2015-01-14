a438712176423b96ebf5b548baa5dd21d94f9765
robojobobobo <robojobobobo@users.noreply.github.com>
2015-01-14 16:51:33 -0500

    Update StringUtils.java

--- src/javasource/communitycommons/StringUtils.java
+++ src/javasource/communitycommons/StringUtils.java
@@ -50,7 +50,21 @@ public class StringUtils
 	
 	public static String hash(String value, int length) throws NoSuchAlgorithmException, DigestException
 	{
-		return String.valueOf(MessageDigest.getInstance(HASH_ALGORITHM).digest(value.getBytes()));
+		MessageDigest digest = null;
+		digest = MessageDigest.getInstance(HASH_ALGORITHM);
+		return bytesToHex(digest.digest(value.getBytes()));
+	}
+	
+	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+	
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
 	}
 
 	public static String regexReplaceAll(String haystack, String needleRegex,
